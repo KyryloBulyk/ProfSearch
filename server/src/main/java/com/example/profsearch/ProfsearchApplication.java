56bf8329a0c46bb4c5ddf18f0b2c0e347cf9f52f
KyryloBulyk <kyrylo.bulyk@gmail.com>
2023-10-01 01:28:04 +0200

    Init server

---
+++ server/src/main/java/com/example/profsearch/ProfsearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.profsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfsearchApplication.class, args);
+	}
+
+}
