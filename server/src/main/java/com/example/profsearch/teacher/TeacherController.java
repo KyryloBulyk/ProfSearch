eb8963c697df7235a0bb251811e08735e30ba6bf
nazar <nazarii.korchevskyi@student.tuke.sk>
2023-10-01 10:30:31 +0200

    added BE with GET request

---
+++ server/src/main/java/com/example/profsearch/teacher/TeacherController.java
@@ -0,0 +1,27 @@
+package com.example.profsearch.teacher;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/teacher")
+public class TeacherController {
+    private final TeacherService teacherService;
+
+    @Autowired
+    public TeacherController(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
+
+    @GetMapping
+    public List<Teacher> getTeachers() {
+        return teacherService.getTeachers();
+    }
+
+    @PostMapping
+    public void registerTeacher(@RequestBody Teacher teacher) {
+        teacherService.addTeacher(teacher);
+    }
+}
