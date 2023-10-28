0ac9dbcc8d1c763fd001c53edfcd70682ce6c0af
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2023-10-28 17:15:05 +0200

    Moved interfaces into the separate file

---
+++ admin/src/types.tsx
@@ -0,0 +1,15 @@
+export interface ITeacher {
+  _id: number;
+  name: string;
+  surname: string;
+  title: string;
+  photoUrl?: string;
+  room: string;
+  building: string;
+  department: string;
+  email: string;
+  linkedIn?: string;
+  instagram?: string;
+  description_UA: string;
+  description_SK: string;
+}
